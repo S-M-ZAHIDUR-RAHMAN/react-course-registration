e04a2d390483c0ab274eaa5a64260b0d3f326540
S-M-ZAHIDUR-RAHMAN <smz.xahid@gmail.com>
2023-09-16 04:17:46 +0600

    Completed Cart without minor issues to solve

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,21 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({ courseName, remaining, totalCredit }) => {
+    // console.log(remaining);
     return (
         <div>
-            <h1>This is cart</h1>
+            <h5>Credit Hour Remaining {remaining} hr</h5>
+            <hr />
+            {courseName.map((course) => (
+                <ol>
+                    <li key={course.id}>{course.name}</li>
+                </ol>
+            ))}
+            <hr />
+            <h5>Total Credit Hour : {totalCredit}</h5>
         </div>
     );
 };
