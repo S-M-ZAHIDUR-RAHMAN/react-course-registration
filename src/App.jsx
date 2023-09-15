f8a737c4fb194fdba7a3abf3790cfd58662eacae
S-M-ZAHIDUR-RAHMAN <smz.xahid@gmail.com>
2023-09-15 16:10:01 +0600

    created and added App.json file and structured components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Main from './components/Main/Main'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl bg-red-400'>Course Registration</h1>
+      <Main></Main>
      
     </>
   )
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+const Main = () => {
+    return (
+        <div>
+            <h1 className='flex justify-center text-3xl font-bold'>Course Registration</h1>
+            <Cart></Cart>
+        </div>
+    );
+};
+
+export default Main;
